d943cd6fb541ca81967866824334df34300e595b
huguds <vfvitinhoblack@gmail.com>
2021-07-13 21:28:20 -0300

    Introduction to variables and operators

---
+++ Exercicio01.py
@@ -0,0 +1,15 @@
+# Programador: Victor Hugo | algoritmo: introdução á váriaveis e operadores | data: 13/07/2021
+
+#váriaveis
+s = 'Asdrubal'; #String = conjunto de caracteres 
+i = 1; #Inteiro = números inteiros: Ex 1|2|3|4...
+f = 1.2; #Decimais(float) = números decimais: Ex 1,2|1,3|1,5...
+b = True; #Boolean(True or False) = tipo de dado primitivo que possui dois valores = verdadeiro(true) ou Falso(False).
+#Operadores
+print('soma:',1+1); # operador de soma +
+print('subtração:',1-1); # operador de subtração -
+print ('multiplicação: ',2*4); # operador de multiplicação *
+print('divisao: ', 5/2); # operador de divisão / (sem arredondar)
+print('divisão: ', 5//2); # operador de divisão // (arredonda para baixo)
+print ('potência: ', 5 ** 2); # operador de potencia **
+print('resto: ',1%2); # Operador que imprime o resto da divisão %
