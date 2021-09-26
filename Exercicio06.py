d0a78b4e0ae7e904c1cedc3c4b88e61878ce1b7b
huguds <vfvitinhoblack@gmail.com>
2021-09-26 18:29:52 -0300

    Revert "Conditional Structures in Python"
    This reverts commit b4fad10875708d1da099ce123af90821903d2de4.

---
+++ Exercicio06.py
@@ -0,0 +1,38 @@
+# Programador: Victor Hugo | algoritmo: Estrutura de Repetição for | data: 05/08/2021
+# O laço de repetição for precisa de uma váriavel de inicio e uma distância para ser percorrida.
+# Sendo utilizado para percorrer listas, dicionários e strings
+# 1 - Faça um algoritmo que conte de 0 até 10
+print(5*"=","Exemplo 1",5*"=")
+x = 1; i = 0; soma_dos_numeros = 0; z = 0;
+for x in range(10):
+    print(x, end=" ")  # end é utilizado para imprimir os valores na mesma linha
+print('\n')
+# 2 - Faça um algoritmo que mostre todos os nomes de um lista
+print(5*"=","Exemplo 2",5*"=")
+nomes = ['Asdrubal','Emengarda','Vladilson']
+for x in nomes:
+    print(x)
+print('\n')
+# 3 - Faça um algoritmo que transforme todos elementos de uma lista em maiusculo
+print(5*"=","Exemplo 3",5*"=")
+for x in nomes:
+    print(x.upper())
+print('\n')
+# 4 - Faça um algoritmo que mostre a quantidade de indices e elementos de uma lista
+print(5*"=","Exemplo 4",5*"=")
+for x in nomes:
+    i+=1
+qtdElements = i
+qtdIndices = i-1
+print(f"O número de elementos é de: {qtdElements}")
+print(f"O número de indices é de: {qtdIndices}")
+print('\n')
+# 5 - Faça um algoritmo que calcule a soma e a média de uma lista
+print(5*"=","Exemplo 5",5*"=")
+numeros = [5,5,5,5,5,5,5,5]
+for x in numeros:
+    z += 1
+    soma_dos_numeros += x
+print(f'A soma dos números é de: {soma_dos_numeros}')
+print(f'A média dos números é de: {soma_dos_numeros/z}')
+print('\n')
